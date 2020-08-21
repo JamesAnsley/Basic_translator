5bbfb14aa751e4b74aeb1e726d21e9321b352899
JamesAnsley <JamesAnsleyGE@gmail.com>
2020-08-21 17:13:35 -0500

    added error handling for FileNotFound

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from translate import Translator
 from datetime import *
+try:
+    org_file = input("What file to translate (include extension please)  ")
+    translator = Translator(to_lang='ja')
+    with open(f'{org_file}', mode='r+') as to_trans:
+        to_trans.seek(0)
+        translation = translator.translate(to_trans.read())
+        print(translation)
+        to_trans.close()
 
-org_file = input("What File to Translate")
-
-translator = Translator(to_lang='ja')
-with open(f'{org_file}.txt', mode='r+') as to_trans:
-    to_trans.seek(0)
-    translation = translator.translate(to_trans.read())
-    print(translation)
-    to_trans.close()
-
-with open(f'.\\translated-files\\{org_file} translated-{datetime.now().strftime("%y%m%d-%H%M%S")}.txt', mode='w') as translated:
-    print(str(translation))
-    translated.close()
+    with open(f'.\\translated-files\\{org_file} translated-{datetime.now().strftime("%y%m%d-%H%M%S")}.txt', mode='w') as translated:
+        print(str(translation))
+        translated.close()
+except FileNotFoundError as err:
+    print('File not found')
